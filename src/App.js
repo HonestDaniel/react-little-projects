7e9b01953821c9f5809f499654a9d29bac0c44e5
Iesha Chromie <ieshachr@va-e3.kzn.21-school.ru>
2022-11-07 22:37:36 +0300

    quiz

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import React from 'react';
 
 const questions = [
   {
@@ -20,39 +21,69 @@ const questions = [
     ],
     correct: 2,
   },
+  {
+    title: 'React - это ... ?',
+    variants: ['библиотека', 'фреймворк', 'приложение'],
+    correct: 0,
+  },
 ];
 
-function Result() {
+function Result({correct, onClickAgain}) {
   return (
     <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <img alt='' src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>Вы отгадали {correct} ответов из {questions.length}</h2>
+      <button onClick={()=>onClickAgain()}>Попробовать снова</button>
     </div>
   );
 }
 
-function Game() {
+function Game({q_index, question, onClickVariant}) {
+  // const [q_index, setQ_index] = React.useState(0);
+  const percentage = Math.round((q_index / questions.length * 100));
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+       {
+          question.variants.map((variant, index) => (
+            <li onClick={() => onClickVariant(index)} key={index}>{variant}</li>
+          ))
+          // questions[q_index].variants.map((variant, index) => (
+          //   <li key={index} 
+          //   onClick={setQ_index(q_index + 1)}>
+          //     {variant}</li>
+          // ))
+        }
       </ul>
     </>
   );
 }
 
 function App() {
+  const [q_index, setQ_index] = React.useState(0);
+  const [correct, updateCorrect] = React.useState(0);
+  const question = questions[q_index];
+
+  const onClickVariant = (index) => {
+    setQ_index(q_index + 1)
+    if (index === question.correct) {
+      updateCorrect(correct + 1)
+    }
+  }
+
+  const onClickAgain = () => {
+    setQ_index(0)
+    updateCorrect(0)
+  }
+
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {q_index === questions.length ? <Result onClickAgain={onClickAgain} correct = {correct}/> : 
+      <Game q_index = {q_index} question = {question} onClickVariant={onClickVariant}/>}
     </div>
   );
 }
