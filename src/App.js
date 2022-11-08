caf255ffbd2ae9167c18bcac6b6628564c10fda5
Iesha Chromie <ieshachr@va-e3.kzn.21-school.ru>
2022-11-08 22:37:54 +0300

    users

--- src/App.js
+++ src/App.js
@@ -6,10 +6,52 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [data, setData] = React.useState([])
+  const [invites, setInvites] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(true)
+  const [success, setSuccess] = React.useState(false)
+  const [searchValue, setSearchValue] = React.useState('')
+  React.useEffect(() => {
+    setIsLoading(true)
+    fetch("https://reqres.in/api/users")
+    .then(res => res.json())
+    .then(json => {
+      setData(json.data)
+      setIsLoading(false)
+    }).catch(err => {
+      console.warn(err);
+    })
+  }, [])
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id))
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  }
+
+  const onClickSuccess = () => {
+    setSuccess(!success)
+  }
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {
+      success && invites.length > 0 ?
+      <Success
+      count = {invites.length}
+      onClickSuccess={onClickSuccess}/>
+      :
+      <Users
+      onClickSuccess={onClickSuccess}
+      invites={invites}
+      onClickInvite={onClickInvite}
+      items={data} 
+      isLoading={isLoading} 
+      searchValue={searchValue} 
+      setSearchValue={setSearchValue}/>
+      }
     </div>
   );
 }
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export const Success = ({ count }) => {
+export const Success = ({onClickSuccess, count }) => {
   return (
-    <div class="success-block">
+    <div className="success-block">
       <img src="/assets/success.svg" alt="Success" />
       <h3>Успешно!</h3>
       <p>Всем {count} пользователям отправлено приглашение.</p>
-      <button className="send-invite-btn">Назад</button>
+      <button className="send-invite-btn"><a style={{ textDecoration: 'none', color: 'white' }} href='/'>Назад</a></button>
+      {/* onClick={onClickSuccess} */}
     </div>
   );
 };
