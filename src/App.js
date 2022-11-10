67016c4e89ce67408d78609d4523283bda8c6389
Iesha Chromie <ieshachr@va-e2.kzn.21-school.ru>
2022-11-10 23:55:01 +0300

    0101010

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 import './index.scss';
 
 function Collection({ name, images }) {
@@ -16,22 +17,53 @@ function Collection({ name, images }) {
 }
 
 function App() {
+  const collectionsRef = React.useRef([])
+  React.useEffect(() => {
+    axios.get(`https://636d09b8ab4814f2b276a7b1.mockapi.io/collections?${categoryId > 0 ? `category=${categoryId}` : ''}`)
+    .then(response => {
+      collectionsRef.current = response.data;
+      // setCollections(response.data)
+    })
+    // fetch('https://636d09b8ab4814f2b276a7b1.mockapi.io/collections')
+    // .then((res) => res.json())
+    // .then((json) => {
+    //   setCollections(json)
+    // })
+  }, [])
+
+  // const [collections, setCollections] = React.useState([])
+  const categories = [
+    'Все',
+    'Горы',
+    'Море',
+    'Архитектура',
+    'Города'
+  ]
+  const [categoryId, setCategoryId] = React.useState(0)
+
+  const onClickCategory = (index) => {
+    setCategoryId(index)
+  }
+
+  console.log(collectionsRef.current[categoryId].name)
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {
+            categories.map((categoryName, index) => (
+              <li onClick={() => onClickCategory(index)} key={index} 
+              className={categoryId === index ? 'active' : ''}>{categoryName}</li>
+            ))
+          }
         </ul>
         <input className="search-input" placeholder="Поиск по названию" />
       </div>
       <div className="content">
         <Collection
-          name="Путешествие по миру"
+          name='NAME'
           images={[
             'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
             'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
